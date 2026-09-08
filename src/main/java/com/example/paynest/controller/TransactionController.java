ba32d2190d34e34733dbe58c69353bb8e2d08324
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-09-08 13:58:38 +0600

    Implement TransactionController and TransactionService for CRUD operations

---
+++ src/main/java/com/example/paynest/controller/TransactionController.java
@@ -0,0 +1,43 @@
+package com.example.paynest.controller;
+
+import com.example.paynest.model.Transaction;
+import com.example.paynest.service.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    //    CRUD : create
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Transaction transaction) {
+        transactionService.create(transaction);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    //    CRUD : read
+    @GetMapping("/{id}")
+    public Transaction findById(@PathVariable Integer id) {
+        return transactionService.findById(id);
+    }
+
+    //    CRUD : update
+    @PutMapping("/{id}")
+    public ResponseEntity<Transaction> updateById(@PathVariable Integer id, @RequestBody Transaction transaction) {
+        return ResponseEntity.ok(transactionService.updateById(id, transaction));
+    }
+
+    //    CRUD : delete
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        transactionService.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/paynest/service/TransactionService.java
@@ -0,0 +1,36 @@
+package com.example.paynest.service;
+
+import com.example.paynest.model.Transaction;
+import com.example.paynest.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction findById(Integer id) {
+        return transactionRepository.findById(id).orElseThrow(() -> new RuntimeException("Transaction not found"));
+    }
+
+    public void create(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public Transaction updateById(Integer id, Transaction transaction) {
+        Transaction existingTransaction = findById(id);
+        existingTransaction.setAmount(transaction.getAmount());
+
+        return transactionRepository.save(existingTransaction);
+    }
+
+    public void deleteById(Integer id) {
+        Transaction existingTransaction = findById(id);
+        transactionRepository.delete(existingTransaction);
+    }
+
+}
