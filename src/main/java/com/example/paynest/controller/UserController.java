ff7e647258c5a8e8179964bb76f09dc9d83881dc
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-09-08 11:58:48 +0600

    controller replaced by rest-controller to resolve view-resolution error

--- src/main/java/com/example/paynest/controller/UserController.java
+++ src/main/java/com/example/paynest/controller/UserController.java
@@ -4,10 +4,9 @@ import com.example.paynest.model.User;
 import com.example.paynest.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
